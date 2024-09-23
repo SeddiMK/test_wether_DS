5436fbcc6cfa02345b10670e7db474e78d4b4659
SeddiMK <web.egorovm@gmail.com>
2024-09-23 16:31:09 +0300

    api_k

--- config.js
+++ config.js
@@ -2,4 +2,4 @@ export const URL_CONF = {
 	URL: 'https://api.openweathermap.org/data/2.5/',
 }
 
-export const API_KEY_CONF = {}
+export const API_KEY_CONF = { API_KEY: '3c2ab57f9f8c15dd6a412a2691437821' }
